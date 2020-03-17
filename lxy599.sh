2084d720b7672c36243665182ce188b8f7221f96
dongfangmu <54091990+dongfangmu@users.noreply.github.com>
2020-03-17 15:27:52 +0800

    Update lxy599.sh

--- lxy599.sh
+++ lxy599.sh
@@ -2,16 +2,31 @@
 PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/bin:/sbin
 export PATH
 
+
 function v2ray(){
-    echo "###   东方木自用   ###"
+    echo "###   v2ray后端一键对接脚本v1.0   ###"
+    echo "###     By Linux_Terminal       ###"
+    echo "###     Update: 2020-03-16      ###"
+
+    echo " "
+    echo -e "\033[41;33m 本功能仅支持Debian 9，请勿在其他系统中运行 \033[0m"
+    echo " "
+    echo "---------------------------------------------------------------------------"
+    echo " "
 
     echo " "
     echo -e "\033[42;37m 请输入节点ID \033[0m 参考格式 42"
     read nodeid
     echo " "
 
+    echo " "
+    echo "---------------------------------------------------------------------------"
+    echo -e "\033[41;33m 请确认下列信息无误，任何失误需要重置操作系统！\033[0m"
     echo -e "\033[42;37m 节点ID \033[0m $nodeid"
     echo " "
+    echo -e "\033[41;33m 回车以继续，ctrl+C退出 \033[0m"
+    echo " "
+    echo "---------------------------------------------------------------------------"
 
     read -n 1
     apt-get update -y
@@ -26,3 +41,28 @@ function v2ray(){
     echo -e "\033[42;37m 安装完成 \033[0m"
 }
 
+
+
+function menu(){
+    echo "###       node tool v1.3       ###"
+    echo "###  By Twitter@Linux_Terminal ###"
+    echo "###    Update: 2020-03-17      ###"
+    echo ""
+
+    echo "---------------------------------------------------------------------------"
+
+    echo -e "\033[42;37m [1] \033[0m 安装v2ray后端"
+    echo -e "\033[41;33m 请输入选项以继续，ctrl+C退出 \033[0m"
+
+    opt=0
+    read opt
+    if [ "$opt"x = "1"x ]; then
+        v2ray
+
+    else
+        echo -e "\033[41;33m 输入错误 \033[0m"
+        bash ./node.sh
+    fi
+}
+
+menu
