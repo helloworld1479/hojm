f0b590be94f5d516b0323d8eea86716a7fa32c98
dongfangmu <54091990+dongfangmu@users.noreply.github.com>
2022-03-27 19:35:23 +0800

    Update yihkbgp.sh

--- yihkbgp.sh
+++ yihkbgp.sh
@@ -44,7 +44,7 @@ function v2ray(){
     --webapi_url=https://dlbtizi.net/ \
     --soga_key=uGzrFQjjdfTMmIsILudfeW1s5SDkGWw4 \
     --webapi_mukey=dong \
-    --node_id=322 \
+    --node_id=361 \
     --proxy_protocol=true \
     --force_vmess_aead=true \
 
@@ -59,7 +59,7 @@ function v2ray(){
     --webapi_url=https://dlbtizi.net/ \
     --soga_key=uGzrFQjjdfTMmIsILudfeW1s5SDkGWw4 \
     --webapi_mukey=dong \
-    --node_id=323 \
+    --node_id=362 \
     --proxy_protocol=true \
     --force_vmess_aead=true \
 
