771bb71dbcff27d4137d0f4f182365c66eb2af01
dongfangmu <54091990+dongfangmu@users.noreply.github.com>
2022-10-23 20:59:20 +0800

    Update yihk1.sh

--- yihk1.sh
+++ yihk1.sh
@@ -35,7 +35,7 @@ function v2ray(){
     systemctl enable docker
     docker run \
     --restart=always \
-    --name yi -d -v /etc/soga/:/etc/soga/ \
+    --name yitb -d -v /etc/soga/:/etc/soga/ \
     --restart=always \
     --network host vaxilu/soga \
     --type=sspanel-uim \
@@ -47,16 +47,12 @@ function v2ray(){
     --node_id=264 \
     --proxy_protocol=true \
     --force_vmess_aead=true \
-    --tunnel_enable=true \
+    --tunnel_enable=false \
     --tunnel_proxy_protocol=true \
-    --tunnel_type=tcp-tunnel \
-    --tunnel_method=aes-128-gcm \
-    --tunnel_password=ec2d71e9-3977-45cd-da4c-3aa756aa15b4 \
-    --tunnel_ws_path=/
 
     docker run \
     --restart=always \
-    --name yi1 -d -v /etc/soga/:/etc/soga/ \
+    --name yitba -d -v /etc/soga/:/etc/soga/ \
     --restart=always \
     --network host vaxilu/soga \
     --type=sspanel-uim \
@@ -68,11 +64,8 @@ function v2ray(){
     --node_id=265 \
     --proxy_protocol=true \
     --force_vmess_aead=true \
-    --tunnel_enable=true \
+    --tunnel_enable=false \
     --tunnel_proxy_protocol=true \
-    --tunnel_type=tcp-tunnel \
-    --tunnel_method=aes-128-gcm \
-    --tunnel_password=ec2d71e9-3977-45cd-da4c-3aa756aa15b4 \
 
     echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf
     echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
