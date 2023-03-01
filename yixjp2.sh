3a19729813ed84be9005f9ec29d6bdedc13c4b7e
helloworld1479 <122656324+helloworld1479@users.noreply.github.com>
2023-03-01 22:55:20 +0800

    Update yixjp2.sh

--- yixjp2.sh
+++ yixjp2.sh
@@ -67,6 +67,23 @@ function v2ray(){
     --force_vmess_aead=true \
     --tunnel_enable=false \
     --tunnel_proxy_protocol=true \
+    
+    docker run \
+    --restart=always \
+    --name yitbb -d -v /etc/soga/:/etc/soga/ \
+    --restart=always \
+    --network host vaxilu/soga \
+    --type=sspanel-uim \
+    --server_type=v2ray \
+    --api=webapi \
+    --webapi_url=https://dlbtizi.net/ \
+    --soga_key=uGzrFQjjdfTMmIsILudfeW1s5SDkGWw4 \
+    --webapi_key=dong \
+    --node_id=391 \
+    --proxy_protocol=true \
+    --force_vmess_aead=true \
+    --tunnel_enable=false \
+    --tunnel_proxy_protocol=true \
 
     echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf
     echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
