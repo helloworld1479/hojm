51e95a1309e1ad16a854f280181a31b9232fb7ba
helloworld1479 <122656324+helloworld1479@users.noreply.github.com>
2023-10-11 20:10:23 +0800

    Update weibo2.sh

--- weibo2.sh
+++ weibo2.sh
@@ -1,93 +1,81 @@
-#!/bin/bash
-PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/bin:/sbin
-export PATH
+#!/usr/bin/env bash
+set -o errexit
+set -o nounset
+set -o pipefail
+SPECIFIC_IP="223.113.130.53"  # 将这里的IP地址替换为您想要的特定IP
 
-function v2ray(){
-    echo "###   v2自用后端   ###"
-    echo "###     20210127      ###"
-    echo "###     11      ###"
+CFKEY="754562d8862d840a8eb6009745b79fc352610"
+CFUSER="6733268@gmail.com"
 
-    echo " "
-    echo -e "\033[41;33m 本功能仅支持Debian 9，请勿在其他系统中运行 \033[0m"
-    echo " "
-    echo "---------------------------------------------------------------------------"
-    echo " "
+CFZONE_NAMES=("dfnode.top" "dfnode.top" "dfnode.top" "dfnode.top" "dfnode.top" "dfnode.top" "dfnode.top" "dongfanghl.com")
+CFRECORD_NAMES=("cs" "hkcu" "jpcu" "otcu" "sgpcu9" "e13" "u92" "download2")
 
-    echo " "
-    echo -e "\033[42;37m 请输入节点ID \033[0m 参考格式 42"
-    read nodeid
-    echo " "
+CFRECORD_TYPE=A
+CFTTL=1
+FORCE=false
+WANIPSITE="http://ipv4.icanhazip.com"
 
-    echo " "
-    echo "---------------------------------------------------------------------------"
-    echo -e "\033[41;33m 请确认下列信息无误，任何失误需要重置操作系统！\033[0m"
-    echo -e "\033[42;37m 节点ID \033[0m $nodeid"
-    echo " "
-    echo -e "\033[41;33m 回车以继续，ctrl+C退出 \033[0m"
-    echo " "
-    echo "---------------------------------------------------------------------------"
+if [ "$CFRECORD_TYPE" = "A" ]; then
+  :
+elif [ "$CFRECORD_TYPE" = "AAAA" ]; then
+  WANIPSITE="http://ipv6.icanhazip.com"
+else
+  echo "$CFRECORD_TYPE specified is invalid, CFRECORD_TYPE can only be A(for IPv4)|AAAA(for IPv6)"
+  exit 2
+fi
 
-    read -n 1
-    echo "net.ipv6.conf.all.disable_ipv6 = 1" >> /etc/sysctl.conf
-    sysctl -p /etc/sysctl.conf
-    apt-get install sudo
-    sudo apt-get update
-    echo '127.0.0.1       soga.sprov.xyz' | sudo tee -a /etc/hosts
-    echo '127.0.0.1       doc.sprov.xyz' | sudo tee -a /etc/hosts
-    sudo apt-get install \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    gnupg-agent \
-    software-properties-common -y
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-    sudo add-apt-repository \
-    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-    $(lsb_release -cs) \
-    stable"
-    sudo apt-get install docker-ce docker-ce-cli containerd.io -y
-    systemctl start docker
-    systemctl enable docker
-    cd /etc
-    wget https://github.com/dongfangmu/soga/releases/download/2.0.6/so.tar.gz
-    tar -xzf so.tar.gz
-    cd 
-    docker run --restart=on-failure --name soga -d -v /etc/soga/:/etc/soga/ --network host rmanluo/crack-soga \
-    --type=sspanel-uim \
-    --server_type=v2ray \
-    --api=webapi \
-    --webapi_url=https://dongfangmublog.com/ \
-    --webapi_mukey=dong \
-    --node_id=$nodeid \
-    --proxy_protocol=true \
-    --soga_key=uGzrFQjjdfTMmIsILudfeW1s5SDkGWw4
-    echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf
-    echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
-    sysctl -p
-    echo " "
-    echo " "
-    echo -e "\033[42;37m 安装完成 \033[0m"
-}
+for i in "${!CFZONE_NAMES[@]}"; do
+    CFZONE_NAME="${CFZONE_NAMES[$i]}"
+    CFRECORD_NAME="${CFRECORD_NAMES[$i]}"
 
-function menu(){
-    echo "###         东方木自用          ###"
-    echo "###            雪域宝贝         ###"
-    echo "###    Update: 2021-01-27      ###"
-    echo ""
-
-    echo "---------------------------------------------------------------------------"
-
-    echo -e "\033[42;37m [1] \033[0m 安装v2ray后端"
-    echo -e "\033[41;33m 请输入选项以继续，ctrl+C退出 \033[0m"
-
-    opt=0
-    read opt
-    if [ "$opt"x = "1"x ]; then
-        v2ray
-
-    else
-        v2ray
+    if [ "$CFRECORD_NAME" != "$CFZONE_NAME" ] && ! [ -z "${CFRECORD_NAME##*$CFZONE_NAME}" ]; then
+        CFRECORD_NAME="$CFRECORD_NAME.$CFZONE_NAME"
+        echo " => Hostname is not a FQDN, assuming $CFRECORD_NAME"
     fi
-}
 
-menu
+    WAN_IP=$SPECIFIC_IP
+    WAN_IP_FILE=$HOME/.cf-wan_ip_$CFRECORD_NAME.txt
+    if [ -f $WAN_IP_FILE ]; then
+      OLD_WAN_IP=`cat $WAN_IP_FILE`
+    else
+      echo "No file, need IP"
+      OLD_WAN_IP=""
+    fi
+
+    if [ "$WAN_IP" = "$OLD_WAN_IP" ] && [ "$FORCE" = false ]; then
+      echo "WAN IP Unchanged, to update anyway use flag -f true"
+      exit 0
+    fi
+
+    ID_FILE=$HOME/.cf-id_$CFRECORD_NAME.txt
+    if [ -f $ID_FILE ] && [ $(wc -l $ID_FILE | cut -d " " -f 1) == 4 ] \
+      && [ "$(sed -n '3,1p' "$ID_FILE")" == "$CFZONE_NAME" ] \
+      && [ "$(sed -n '4,1p' "$ID_FILE")" == "$CFRECORD_NAME" ]; then
+        CFZONE_ID=$(sed -n '1,1p' "$ID_FILE")
+        CFRECORD_ID=$(sed -n '2,1p' "$ID_FILE")
+    else
+        echo "Updating zone_identifier & record_identifier"
+        CFZONE_ID=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones?name=$CFZONE_NAME" -H "X-Auth-Email: $CFUSER" -H "X-Auth-Key: $CFKEY" -H "Content-Type: application/json" | grep -Po '(?<="id":")[^"]*' | head -1 )
+        CFRECORD_ID=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones/$CFZONE_ID/dns_records?name=$CFRECORD_NAME" -H "X-Auth-Email: $CFUSER" -H "X-Auth-Key: $CFKEY" -H "Content-Type: application/json"  | grep -Po '(?<="id":")[^"]*' | head -1 )
+        echo "$CFZONE_ID" > $ID_FILE
+        echo "$CFRECORD_ID" >> $ID_FILE
+        echo "$CFZONE_NAME" >> $ID_FILE
+        echo "$CFRECORD_NAME" >> $ID_FILE
+    fi
+
+    echo "Updating DNS to $WAN_IP"
+    UPDATE=$(curl -s -X PUT "https://api.cloudflare.com/client/v4/zones/$CFZONE_ID/dns_records/$CFRECORD_ID" \
+    -H "X-Auth-Email: $CFUSER" \
+    -H "X-Auth-Key: $CFKEY" \
+    -H "Content-Type: application/json" \
+    --data '{"type":"'$CFRECORD_TYPE'","name":"'$CFRECORD_NAME'","content":"'$WAN_IP'","ttl":'$CFTTL'}' \
+    | grep -Po '(?<="success":)[^,]*')
+
+    if [ $UPDATE = "true" ]; then
+      echo "Updated succesfuly!"
+      echo $WAN_IP > $WAN_IP_FILE
+    else
+      echo 'Something went wrong, check API calls!'
+      exit 1
+    fi
+done
