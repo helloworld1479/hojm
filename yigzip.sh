14c64206edba162c2dc0f0301da1007c7f8e4780
helloworld1479 <122656324+helloworld1479@users.noreply.github.com>
2024-04-12 19:47:57 +0800

    Update yigzip.sh

--- yigzip.sh
+++ yigzip.sh
@@ -7,8 +7,8 @@ SPECIFIC_IP="211.91.66.241"  # 将这里的IP地址替换为您想要的特定IP
 CFKEY="754562d8862d840a8eb6009745b79fc352610"
 CFUSER="6733268@gmail.com"
 
-CFZONE_NAMES=("potatoeswater.com" "dfnode.top" "potatoeswater.com" "dfnode.top" "potatoeswater.com" "dongfanghl.com" "dongfanghl.com" "dongfanghl.com")
-CFRECORD_NAMES=("downca" "jjm" "download1" "jpcm" "download4" "down9pa" "down3a" "download1")
+CFZONE_NAMES=("potatoeswater.com" "dfnode.top" "potatoeswater.com" "dfnode.top" "potatoeswater.com" "dongfanghl.com" "dongfanghl.com" "dongfanghl.com" "dongfanghl.com")
+CFRECORD_NAMES=("downca" "jjm" "download1" "jpcm" "download4" "down9pa" "down3a" "am" "ccu")
 
 CFRECORD_TYPE=A
 CFTTL=1
