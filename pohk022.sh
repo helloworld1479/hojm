a74652a2c6487fa1c466c7c82d924311793127e6
helloworld1479 <122656324+helloworld1479@users.noreply.github.com>
2024-10-22 11:57:50 +0800

    Update pohk022.sh

--- pohk022.sh
+++ pohk022.sh
@@ -33,22 +33,25 @@ function v2ray(){
     sudo apt-get install docker-ce docker-ce-cli containerd.io -y
     systemctl start docker
     systemctl enable docker
-    docker run \
-    --restart=always \
-    --name yitb -d -v /etc/soga/:/etc/soga/ \
-    --restart=always \
-    --network host vaxilu/soga:2.8.9 \
-    --type=sspanel-uim \
-    --server_type=v2ray \
-    --api=webapi \
-    --webapi_url=https://dlbtizi.net/ \
-    --soga_key=uGzrFQjjdfTMmIsILudfeW1s5SDkGWw4 \
-    --webapi_key=dong \
-    --node_id=338 \
-    --proxy_protocol=true \
-    --force_vmess_aead=true \
-    --tunnel_enable=false \
-    --tunnel_proxy_protocol=true \
+    docker run --restart=always --name yitba -d \
+    -v /etc/soga/:/etc/soga/ --network host \
+    -e type=sspanel-uim \
+    -e server_type=v2ray \
+    -e node_id=338 \
+    -e soga_key=uGzrFQjjdfTMmIsILudfeW1s5SDkGWw4 \
+    -e api=webapi \
+    -e webapi_url=https://dlbtizi.net/ \
+    -e webapi_key=dong \
+    -e proxy_protocol=true \
+    -e force_vmess_aead=true \
+    -e tunnel_proxy_protocol=true \
+    -e redis_enable=true \
+    -e redis_addr=ip.dlbtizi.net:1357 \
+    -e redis_password=damai \
+    -e redis_db=0 \
+    -e conn_limit_expiry=60 \
+    -e user_conn_limit=6 \
+    vaxilu/soga:2.10.7
     
     echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf
     echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
